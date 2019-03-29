21dd337cf7fe7e136b1522ae983b451f66d8b173
ForseIKomar <33393253+ForseIKomar@users.noreply.github.com>
2019-03-29 20:59:15 +0300

    start

---
+++ МПС/MPS/Lab_1/AvrBuild.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "C:\Users\Aleksandr\Desktop\MPS\Lab_1\labels.tmp" -fI -W+ie -C V2E -o "C:\Users\Aleksandr\Desktop\MPS\Lab_1\Lab_1.hex" -d "C:\Users\Aleksandr\Desktop\MPS\Lab_1\Lab_1.obj" -e "C:\Users\Aleksandr\Desktop\MPS\Lab_1\Lab_1.eep" -m "C:\Users\Aleksandr\Desktop\MPS\Lab_1\Lab_1.map" "C:\Users\Aleksandr\Desktop\MPS\Lab_1\Lab_1.asm"
