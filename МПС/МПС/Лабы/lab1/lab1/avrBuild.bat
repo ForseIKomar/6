e56cc70dbfcae2add4f7ed48143147d532f228c6
ForseIKomar <33393253+ForseIKomar@users.noreply.github.com>
2019-05-30 18:56:06 +0300

    1

--- МПС/МПС/Лабы/lab1/lab1/avrBuild.bat
+++
@@ -1,5 +0,0 @@
-cd "D:\IU6_71\lab1\"
-D:
-del lab1.map
-del lab1.lst
-"C:\Program Files\Atmel\AVR Tools\AvrAssembler\avrasm32.exe" -fI "D:\IU6_71\lab1\lab1.asm" -o "lab1.hex" -d "lab1.obj" -e "lab1.eep" -I "D:\IU6_71\lab1" -I "C:\Program Files\Atmel\AVR Tools\AvrAssembler\AppNotes" -w  -m "lab1.map"
--- МПС/МПС/Лабы/lab1/lab2/avrBuild.bat
+++
@@ -1,5 +0,0 @@
-cd "D:\IU6_71\lab2\"
-D:
-del lab2.map
-del lab2.lst
-"C:\Program Files\Atmel\AVR Tools\AvrAssembler\avrasm32.exe" -fI "D:\IU6_71\lab2\lab2.asm" -o "lab2.hex" -d "lab2.obj" -e "lab2.eep" -I "D:\IU6_71\lab2" -I "C:\Program Files\Atmel\AVR Tools\AvrAssembler\AppNotes" -w  -m "lab2.map"
